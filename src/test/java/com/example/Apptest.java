0c6daf082192f483cfba2fcaa49f7fad5b7b9460
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-14 12:36:35 +0530

    Maven project 1

--- src/test/java/com/example/Apptest.java
+++ src/test/java/com/example/Apptest.java
@@ -1,7 +1,8 @@
 package com.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class AppTest {
+public class AppTest 
+{
  @Test
  public void testAdd() {
  App app = new App();
