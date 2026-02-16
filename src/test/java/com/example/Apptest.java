458a8bdc1105cdfcebf8471f9c7ed103cffbf8e0
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-16 12:59:23 +0530

    new maven

--- src/test/java/com/example/Apptest.java
+++ src/test/java/com/example/Apptest.java
@@ -1,7 +1,7 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
 
